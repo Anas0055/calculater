e22f9c925fe7e191da390b68d1fc8e47a67ab185
Anas0055 <manaskhan969@gmail.com>
2023-12-23 03:15:29 -0800

    calculator

---
+++ index.js
@@ -0,0 +1,13 @@
+"use strict";
+let a = prompt("enter value for a");
+let b = prompt("enter value for a");
+let c = a + b;
+let d = a - b;
+let e = a * b;
+let g = a / b;
+let f = a % b;
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(g);
+console.log(f);
